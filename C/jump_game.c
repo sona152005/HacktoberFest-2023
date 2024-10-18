fb39b7494f6509d69300d8056fed667f12415693
sona152005 <sonasasikumar152005@gmail.com>
2024-10-18 17:57:32 +0530

    Update jump_game.c

--- C/jump_game.c
+++ C/jump_game.c
@@ -1,42 +1,36 @@
+// modified code in short and add some features
 #include <stdio.h>
 #include <stdbool.h>
 
-bool canJump(int *nums, int numsSize)
-{
+bool canJump(int *nums, int numsSize) {
     int maxReach = 0;
 
-    for (int i = 0; i < numsSize; i++)
-    {
-        // If we've reached a position that is not reachable, return false
-        if (i > maxReach)
-        {
-            return false;
-        }
+    for (int i = 0; i < numsSize; i++) {
+        if (i > maxReach) return false; 
 
-        // Update the maximum reachable position
         maxReach = (i + nums[i] > maxReach) ? (i + nums[i]) : maxReach;
 
-        // If we can reach the end of the array, return true
-        if (maxReach >= numsSize - 1)
-        {
-            return true;
-        }
+        printf("Index: %d, Max Reach: %d\n", i, maxReach);  
+        if (maxReach >= numsSize - 1) return true; 
     }
 
     return false;
 }
 
-int main()
-{
-    int nums[] = {2, 3, 1, 1, 4}; // Example input array
-    int numsSize = sizeof(nums) / sizeof(nums[0]);
+int main() {
+    int nums[100], numsSize;
 
-    if (canJump(nums, numsSize))
-    {
-        printf("Yes, you can reach the end of the array!\n");
+    printf("Enter the number of elements in the array: ");
+    scanf("%d", &numsSize);
+
+    printf("Enter the elements of the array:\n");
+    for (int i = 0; i < numsSize; i++) {
+        scanf("%d", &nums[i]);
     }
-    else
-    {
+
+    if (canJump(nums, numsSize)) {
+        printf("Yes, you can reach the end of the array!\n");
+    } else {
         printf("No, you cannot reach the end of the array.\n");
     }
 
