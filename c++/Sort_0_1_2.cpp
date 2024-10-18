ad8f379b39930f21d82b1ceb34ae22024c8e8fd6
sona152005 <sonasasikumar152005@gmail.com>
2024-10-18 21:29:13 +0530

    Update Sort_0_1_2.cpp

--- c++/Sort_0_1_2.cpp
+++ c++/Sort_0_1_2.cpp
@@ -1,28 +1,23 @@
+// modified this code 
 #include <iostream>
 
 void sortColors(int arr[], int size) {
-    int low = 0;          // Pointer for 0
-    int high = size - 1;  // Pointer for 2
-    int current = 0;      // Pointer for iterating through the array
+    int low = 0, high = size - 1, current = 0;
 
     while (current <= high) {
         if (arr[current] == 0) {
-            std::swap(arr[current], arr[low]);
-            current++;
-            low++;
+            std::swap(arr[current++], arr[low++]);
         } else if (arr[current] == 2) {
-            std::swap(arr[current], arr[high]);
-            high--;
+            std::swap(arr[current], arr[high--]);
         } else {
             current++;
         }
     }
 }
 
-void printArray(int arr[], int size) {
-    for (int i = 0; i < size; i++) {
+void printArray(const int arr[], int size) {
+    for (int i = 0; i < size; i++)
         std::cout << arr[i] << " ";
-    }
     std::cout << std::endl;
 }
 
