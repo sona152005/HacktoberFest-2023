256744c66855d936b08f01bc2b8a4adfb59df862
sona152005 <sonasasikumar152005@gmail.com>
2024-10-18 21:10:02 +0530

    Update Dimond_pattern.cpp

--- c++/Dimond_pattern.cpp
+++ c++/Dimond_pattern.cpp
@@ -1,34 +1,22 @@
+// modified this code in short
 #include <iostream>
 using namespace std;
 
 int main() {
     int n;
-    cout << "Enter the number of rows (should be an odd number): ";
+    cout << "Enter an odd number of rows: ";
     cin >> n;
 
     if (n % 2 == 0) {
-        cout << "Please enter an odd number for a symmetric diamond pattern."<<endl;
+        cout << "Please enter an odd number." << endl;
         return 1;
     }
 
     for (int i = 1; i <= n; i += 2) {
-        for (int j = 0; j < (n - i) / 2; j++) {
-            cout << " ";
-        }
-        for (int j = 0; j < i; j++) {
-            cout << "*";
-        }
-        cout <<endl;
+        cout << string((n - i) / 2, ' ') << string(i, '*') << endl;
     }
-
     for (int i = n - 2; i >= 1; i -= 2) {
-        for (int j = 0; j < (n - i) / 2; j++) {
-            cout << " ";
-        }
-        for (int j = 0; j < i; j++) {
-            cout << "*";
-        }
-        cout <<endl;
+        cout << string((n - i) / 2, ' ') << string(i, '*') << endl;
     }
 
     return 0;
