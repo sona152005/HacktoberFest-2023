1b67940b70886ccb8e1c8df13751f1a0b01cd0ea
sona152005 <sonasasikumar152005@gmail.com>
2024-10-19 09:02:59 +0530

    Update Bubble_Sort.java

--- java/Bubble_Sort.java
+++ java/Bubble_Sort.java
@@ -1,14 +1,31 @@
-public class Bubble_Sort {
+// modified this code
+public class BubbleSort {
     public int[] sortArray(int[] nums) {
-        for (int i = 0; i < nums.length - 1; i++) {
-            for (int j = 0; j < nums.length - i - 1; j++) {
+        int n = nums.length;
+        for (int i = 0; i < n - 1; i++) {
+            boolean swapped = false; // Track if a swap occurred
+            for (int j = 0; j < n - i - 1; j++) {
                 if (nums[j] > nums[j + 1]) {
+                    // Swap
                     int temp = nums[j];
                     nums[j] = nums[j + 1];
                     nums[j + 1] = temp;
+                    swapped = true; // Set flag to true
                 }
             }
+            if (!swapped) break; // If no swaps, array is sorted
         }
         return nums;
+    }
+
+    public static void main(String[] args) {
+        BubbleSort sorter = new BubbleSort();
+        int[] nums = {64, 34, 25, 12, 22, 11, 90};
+        int[] sortedArray = sorter.sortArray(nums);
+        
+        System.out.println("Sorted array: ");
+        for (int num : sortedArray) {
+            System.out.print(num + " ");
+        }
     }
 }
